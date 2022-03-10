177d7b301ce2ebf5a30f496922d5a331013ef314
fei-ke <jinyang656@qq.com>
2022-03-10 10:57:55 +0800

    adapt vendor and product

---
+++ post-fs-data.sh
@@ -0,0 +1,24 @@
+#!/system/bin/sh
+# Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
+# This will make your scripts compatible even if Magisk change its mount point in the future
+MODDIR=${0%/*}
+
+if [ -e /system/etc/permissions/services.cn.google.xml ]; then
+    origin=/system/etc/permissions/services.cn.google.xml
+elif [ -e /vendor/etc/permissions/services.cn.google.xml ]; then
+    origin=/vendor/etc/permissions/services.cn.google.xml
+elif [ -e /product/etc/permissions/services.cn.google.xml ]; then
+    origin=/product/etc/permissions/services.cn.google.xml
+else
+    exit 0
+fi
+
+if [[ $origin == *system* ]]; then
+    target=$MODDIR$origin
+else
+    target=$MODDIR/system$origin
+fi
+
+mkdir -p $(dirname $target)
+cp -f $origin $target
+sed -i '/cn.google.services/d' $target
